1e1b9fb5368b0243cce30b7d8ff8848e07748aa9
Karen Farmanyan <farmanian.k@yandex.ru>
2024-03-17 17:27:23 +0300

    #11 Correct exception handler

--- src/main/java/org/otus/platform/gateway/service/userservice/profile/UserProfileServiceImpl.java
+++ src/main/java/org/otus/platform/gateway/service/userservice/profile/UserProfileServiceImpl.java
@@ -3,6 +3,7 @@ package org.otus.platform.gateway.service.userservice.profile;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.otus.platform.gateway.client.UserClient;
+import org.otus.platform.gateway.dto.userservice.UserDto;
 import org.otus.platform.gateway.dto.userservice.userprofile.UpdateUserRequest;
 import org.otus.platform.gateway.dto.userservice.userprofile.UpdateUserResponse;
 import org.otus.platform.gateway.mapper.userservice.UserMapper;
@@ -31,5 +32,17 @@ public class UserProfileServiceImpl implements UserProfileService {
         log.info("invoke updateUserRole() method");
         var userDto = userClient.updateUserRole(id, role).getBody();
         return userMapper.toUpdateUserResponse(userDto);
+    }
+
+    @Override
+    public UserDto getUserById(UUID id) {
+        log.info("invoke getUserById() method");
+        return userClient.getUserById(id).getBody();
+    }
+
+    @Override
+    public UserDto getUserByEmail(String email) {
+        log.info("invoke getUserByEmail() method");
+        return userClient.getUserByEmail(email).getBody();
     }
 }
